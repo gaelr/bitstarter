7517db87ff812908f99cfe4a55045810dd138c68
Gael <gael.reignier@gmail.com>
2013-07-09 05:46:04 +0000

    Reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require("fs");
+
+var file = fs.readFile("index.html");
+file = file.buf.toString();
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
